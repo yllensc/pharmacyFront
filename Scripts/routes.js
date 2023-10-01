92a0094845ecee694403990948ba7395d1699e30
Juan <lozadagomezj@gmail.com>
2023-10-01 04:15:07 -0500

    feat: :sparkles: Creacion de sale(tiene un pequeñito error pero lo estoy corrigiendo)

--- Scripts/routes.js
+++ Scripts/routes.js
@@ -18,6 +18,7 @@ export const end35 = "http://localhost:5223/api/pharmacy/Provider/aleast5medicin
 
 // SALES
 export const getSales = "http://localhost:5223/api/pharmacy/Sale";
+export const postManySales = "http://localhost:5223/api/pharmacy/Sale/range";
 export const end5 = "http://localhost:5223/api/pharmacy/Sale/totalSaleOneMedicine";
 export const end8 = "http://localhost:5223/api/pharmacy/Sale/gainSales";
 export const end9 = "http://localhost:5223/api/pharmacy/Sale/unsoldMedicines2023";
@@ -32,6 +33,13 @@ export const end10 = "http://localhost:5223/api/pharmacy/Medicine/moreExpensive"
 
 //MEDICINES
 export const getMedicines = "http://localhost:5223/api/pharmacy/Medicine/";
+
+//EMPLOYEES
+export const getEmployees = "http://localhost:5223/api/pharmacy/Employee/";
+
+
+//PATIENTS
+export const getPatients = "http://localhost:5223/api/pharmacy/Patient/";
 
 /* Routes HTML */
 export const indexRoute = "../Modules/index.js"; 
